0f8b9d0708187e0b5bd5f08067496897de63979d
BeyKun <bayurasukmaraga@gmail.com>
2021-02-11 11:31:01 +0700

    custom text

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ wa.create().then(client => start(client));
 
 function start(client) {
   client.onMessage(async message => {
+    
+    client.sendText(message.from, `
+        Helllo, disini Afedigi!!
+        Ada yang bisa saya bantu?????
+        
+        - Nindi
+    `)
+
+
     if (message.body === 'Hi') {
       await client.sendText(message.from, '👋 Hello!');
     }
